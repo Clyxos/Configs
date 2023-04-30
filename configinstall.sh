6d1291022e49e1a101202666a69455358b69badb
Clyxos <clyxosisgamer@gmail.com>
2023-04-30 14:21:42 -0400

    Setting up just configinstall.sh

---
+++ configinstall.sh
@@ -0,0 +1,13 @@
+# Setup directories
+mkdir /home/clyxos/.config
+mkdir /home/clyxos/.config/bspwm
+mkdir /home/clyxos/.config/sxhkd
+mkdir /home/clyxos/.config/picom
+mkdir /home/clyxos/.config/polybar
+# Put the configs into place
+cp bspwmrc /home/clyxos/.config/bspwm/
+cp sxhkdrc /home/clyxos/.config/sxhkd
+cp picom.conf /home/clyxos/.config/picom/
+cp config.ini /home/clyxos/.config/polybar/
+cp launch.sh /home/clyxos/.config/polybar/
+
